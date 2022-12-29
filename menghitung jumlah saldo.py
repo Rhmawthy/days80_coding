b1ed292afcc096a015a86c300be5e311923bd8ca
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-12-29 09:56:48 +0800

    Add files via upload

---
+++ menghitung jumlah saldo.py
@@ -0,0 +1,10 @@
+saldo = int(input("masukkan jumlah saldo anda : "))
+
+penarikan = int(input("masukkan jumlah penarikan : "))
+
+rumus = saldo - penarikan
+
+print("saldo anda = ",saldo)
+print ("anda melakukan penarikan sebesar =" ,penarikan)
+
+print("sisah saldo anda",rumus)
